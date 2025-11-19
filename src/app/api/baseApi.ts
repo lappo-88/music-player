96bbeefd373cfa1d7eb79d03789bdd3664f62f77
lappo-88 <mikhail08062025@gmail.com>
2025-11-19 22:04:55 +0300

    error handling(status code 403,401,500)

--- src/app/api/baseApi.ts
+++ src/app/api/baseApi.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import {toast} from "react-toastify";
-import { isErrorWithProperty} from "@/common/utils";
+import {isErrorWithDetailArray, isErrorWithProperty, trimToMaxLength} from "@/common/utils";
 
 export const baseApi = createApi({
     reducerPath: 'baseApi',
@@ -9,7 +9,7 @@ export const baseApi = createApi({
         const result = await fetchBaseQuery({
             baseUrl: import.meta.env.VITE_BASE_URL,
             headers: {
-                'API-KEY': import.meta.env.VITE_API_KEY + "fgh",
+                'API-KEY': import.meta.env.VITE_API_KEY,
             },
             prepareHeaders: headers => {
                 headers.set('Authorization', `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`)
@@ -19,6 +19,12 @@ export const baseApi = createApi({
 
         if (result.error) {
             switch (result.error.status) {
+                case 'FETCH_ERROR':
+                case 'PARSING_ERROR':
+                case 'CUSTOM_ERROR':
+                case 'TIMEOUT_ERROR':
+                    toast(result.error.error, { type: 'error', theme: 'colored' })
+                    break
                 case 404:
                     if (isErrorWithProperty(result.error.data, "error")) {
                         toast(result.error.data.error, {type: 'error', theme: 'colored'})
@@ -26,7 +32,7 @@ export const baseApi = createApi({
                         toast(JSON.stringify(result.error.data), {type: 'error', theme: 'colored'})
                     }
                     break
-
+                case 401:
                 case 429:
                     if (isErrorWithProperty(result.error.data, "message")) {
                         toast(result.error.data.message, {type: 'error', theme: 'colored'})
@@ -35,8 +41,21 @@ export const baseApi = createApi({
                     }
                     break
 
+                case 400:
+                case 403:
+                    if (isErrorWithDetailArray(result.error.data)) {
+                        toast(trimToMaxLength(result.error.data.errors[0].detail), { type: "error", theme: "colored" })
+                    } else {
+                        toast(JSON.stringify(result.error.data), { type: "error", theme: "colored" })
+                    }
+                    break
+
                 default:
-                    toast('Some error occurred', {type: 'error', theme: 'colored'})
+                    if (result.error.status >= 500 && result.error.status < 600) {
+                        toast("Server error occurred. Please try again later.", { type: "error", theme: "colored" })
+                    } else {
+                        toast("Some error occurred", {type: "error", theme: "colored"})
+                    }
             }
         }
         return result
